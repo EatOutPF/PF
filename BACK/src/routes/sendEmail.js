0f4a543d8b2daa6face7c78db4d1fe8334c252de
Barbara Denise <miranda.debarbi@gmail.com>
2023-04-04 19:18:53 -0300

    confirmacion por email al restaurant cuando se la orden de pago

--- BACK/src/routes/sendEmail.js
+++ BACK/src/routes/sendEmail.js
@@ -1,43 +1,18 @@
 const { Router } = require("express");
-const nodemailer = require("nodemailer");
 const express = require("express");
+const { sendConfirmationEmail } = require("../controllers/controllerEmail");
 const router = Router();
 router.use(express.json());
-const { EMAIL, KEY_GMAIL } = process.env;
 
+router.post("/", async (req, res) => {
+  const { email, name, price } = req.query;
+  try {
 
-router.post("/", (req, res) => {
-  const {email} = req.body;
-  
-
-  // Configurar el transporter con los detalles del servicio de correo electrónico
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: EMAIL, // Dirección de correo electrónico del remitente
-      pass: KEY_GMAIL, // Contraseña de la cuenta de correo electrónico del remitente
-    },
-  });
-
-  // Crear la plantilla del correo electrónico
-  let mailOptions = {
-    from: "eatoutpf@gmail.com",
-    to: email,
-    subject: "Confirmación de respuesta",
-    text: "Gracias por tu respuesta.",
-  };
-
-  // Enviar el correo electrónico utilizando el transporter y la plantilla creados
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        res.status(400).json({ error: error.message });
-      } else {
-        res.status(200).json("Correo electrónico enviado: " + info.response);
-      }
-  
-  });
-
-
+     await sendConfirmationEmail(email, name, price);
+    res.status(200).json("Se envio la confirmacion");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
