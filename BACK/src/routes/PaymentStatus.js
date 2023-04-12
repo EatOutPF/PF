3ccbf469c8cee8e1e9967980b5a8b2ef023c9df5
EatOutPF <127988631+EatOutPF@users.noreply.github.com>
2023-04-12 19:49:37 -0300

    Update PaymentStatus.js

--- BACK/src/routes/PaymentStatus.js
+++ BACK/src/routes/PaymentStatus.js
@@ -14,21 +14,21 @@ router.post("/", (req, res) => {
     console.log(req.body);
 
 const { body } = req;
-  const signature = req.header('x-signature');
+//  const signature = req.header('x-signature');
 
   // Verifica la firma de la solicitud
-  const hash = crypto.createHmac('sha256', MERCADOPAGO_KEY)
-    .update(JSON.stringify(body))
-    .digest('hex');
+  //const hash = crypto.createHmac('sha256', MERCADOPAGO_KEY)
+    //.update(JSON.stringify(body))
+    //.digest('hex');
 
-  if (hash === signature) {
+  //if (hash === signature) {
     // La firma es válida, procesa el pago y actualiza el estado de la orden de compra
     // ...
     // Retorna una respuesta exitosa para indicar a MercadoPago que la solicitud fue procesada correctamente
     res.status(200).json(body);
-  } else {
+  //} else {
     // La firma es inválida, rechaza la solicitud
-    res.sendStatus(400);
+    //res.sendStatus(400);
   }
 
 });
