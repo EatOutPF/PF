cb0a8595b460e20ea0c7de34466d17bda6871f56
EatOutPF <127988631+EatOutPF@users.noreply.github.com>
2023-04-16 03:47:31 -0300

    Update controllerPaymentSatus.js

--- BACK/src/controllers/controllerPaymentSatus.js
+++ BACK/src/controllers/controllerPaymentSatus.js
@@ -1,32 +1,68 @@
 const express = require('express')
+const axios = require('axios')
 const dotenv = require('dotenv')
 
-async function webhook(props) {
-    console.log(props)
+async function webhook(id) {
+ 
     const token = process.env.MERCADOPAGO_KEY
-    
-    let id = props.payment_id === 'null' ? props.preference_id : props.payment_id
+    console.log(id)
+
     let data
-    
     try {
-        if(!id.includes('-')) {
-        let url = `https://api.mercadopago.com/v1/payments/${id}?access_token=${token}`;
-        let response = await axios 
-        .get(url)
-        .then(res => data = res.data )
-        axios.post(`/sendemail`, { email: "cditoro@gmail.com", name : data, price: 1200})
-        } else {
-            let url =   `https://api.mercadopago.com/checkout/preferences/${id}?access_token=${token}` 
-            let response = await axios 
-        .get(url)
-        .then(res => data = res.data )
+
+        let url = `https://api.mercadopago.com/v1/payments/search?sort=id&criteria=desc&external_reference=${id}&range=date_created&begin_date=NOW-30DAYS&end_date=NOW&access_token=${token}`
+        let response = await axios.get(url)
+        .then(res => data = res.data)
+        console.log(data)
+        // if(!id.includes('-')) {
+        // let url = `https://api.mercadopago.com/v1/payments/${id}?access_token=${token}`;
+        // let response = await axios 
+        // .get(url)
+        // .then(res => data = res.data )
+        // console.log(data);
+        // } else {
+        //     let url =   `https://api.mercadopago.com/checkout/preferences/${id}?access_token=${token}` 
+        //     let response = await axios 
+        // .get(url)
+        // .then(res => data = res.data )
         
-        console.log(data);
-        }
+        // console.log(data);
+        // }
 
         return data
     } catch (err) {
         throw new Error(err)
     }
     }
+
+// async function webhook(props) {
+//     console.log(props)
+//     const token = process.env.MERCADOPAGO_KEY
+    
+//     let id = props.payment_id === 'null' ? props.preference_id : props.payment_id
+//     let data
+    
+//     try {
+//         if(!id.includes('-')) {
+//         let url = `https://api.mercadopago.com/v1/payments/${id}?access_token=${token}`;
+//         let response = await axios 
+//         .get(url)
+//         .then(res => data = res.data )
+//         axios.post(`/sendemail`, { email: "cditoro@gmail.com", name : data, price: 1200})
+//         } else {
+//             let url =   `https://api.mercadopago.com/checkout/preferences/${id}?access_token=${token}` 
+//             let response = await axios 
+//         .get(url)
+//         .then(res => data = res.data )
+        
+//         console.log(data);
+//         }
+
+//         return data
+//     } catch (err) {
+//         throw new Error(err)
+//     }
+//     }
+
+
 module.exports =  webhook 
