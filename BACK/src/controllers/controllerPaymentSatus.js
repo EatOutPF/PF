3d5885119f758d3bf24c51575db180ad0d874b61
EatOutPF <127988631+EatOutPF@users.noreply.github.com>
2023-04-18 11:21:57 -0300

    Update controllerPaymentSatus.js

--- BACK/src/controllers/controllerPaymentSatus.js
+++ BACK/src/controllers/controllerPaymentSatus.js
@@ -110,42 +110,6 @@ async function webhook(reference) {
       });
     }
     let useract = await getUsers(idUser)
-         .populate("restaurant")
-        .populate({
-          path: "favorite",
-          populate: {
-            path: "restaurant",
-            select: "name _id",
-          },
-        })
-        .populate({
-          path: "reserve",
-          populate: {
-            path: "restaurant",
-            select: "_id name address contact",
-          },
-        })
-        .populate({
-          path: "payment",
-          populate: [
-            {
-            path: "restaurant",
-            select: "_id name"
-          },
-          {
-            path: "reserve",
-            select: "_id date"
-          }
-        ]
-        })
-        .populate({
-          path: "review",
-          populate: {
-            path: "restaurant",
-            select: "_id name",
-          },
-        })
-        .populate("notificacion")
 
     return [data.status, useract];
 
